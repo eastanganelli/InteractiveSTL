7691c94fa6652c6d79b44ac5c3d4e5bfad22acfe
eastanganelli <ezequielstanganelli@gmail.com>
2023-04-24 20:27:10 -0300

    Queue update

--- STL/main.js
+++ STL/main.js
@@ -6,7 +6,6 @@ const random_address = () => {
     return  address;
  }
 
-
 // STACK
 let stack = [];
 
@@ -41,4 +40,43 @@ const updateStack = () => {
         stackDiv.append(nodeDiv);
     }
     top_stack();
+}
+
+// QUEUE
+
+let queue = [];
+
+const enqueue = () => {
+    let value = $("#enqueueValue").val();
+    if (value != '') {
+        let address = random_address();
+        queue.push({value: value, address: address});
+        updateQueue();
+        $("#enqueueValue").val('');
+    }
+}
+const dequeue = () => {
+    queue.shift();
+    updateQueue();
+}
+const head_tail_queue = () => {
+    let head_ = $("#queueHead");
+    (queue.length < 1) ? head_.text("nullptr") : head_.text(queue[0].address);
+    let tail_ = $("#queueTail");
+    (queue.length < 1) ? tail_.text("nullptr") : tail_.text(queue[queue.length - 1].address);
+}
+const updateQueue = () => {
+    let queueDiv = $("#queue");
+    queueDiv.empty();
+    for (let i = 0; i < queue.length; i++) {
+        let nodeDiv = $("<div>").addClass("queue-node").text(queue[i].value);
+        let addressDiv = $("<div>").addClass("queue-node-address").append(`<span class="badge text-bg-primary">Address: `+ queue[i].address +`</span>`);
+        if (i < queue.length-1) {
+            let nextAddressDiv = $("<div>").addClass("queue-node-address").append(`<span class="badge text-bg-info">Next: `+ queue[i + 1].address +`</span>`);
+            nodeDiv.append(nextAddressDiv);
+        }
+        nodeDiv.append(addressDiv);
+        queueDiv.append(nodeDiv);
+    }
+    head_tail_queue();
 }
