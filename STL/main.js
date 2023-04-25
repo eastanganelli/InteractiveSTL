34e262a6bc02095abe6f1ebaf296e4659398f311
eastanganelli <ezequielstanganelli@gmail.com>
2023-04-24 21:14:22 -0300

    checked

--- STL/main.js
+++
@@ -1,7 +0,0 @@
-const random_address = () => {
-    return "0x" + Math.floor(Math.random() * 100).toString(16).padStart(4, "0");
-}
-const follow_address = (length) => {
-    let address = length > 0 ? "0x" + length.toString(16).padStart(4, "0") : "null";
-    return  address;
- }
